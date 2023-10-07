7d556485ea3f9eb5c522a9ef8e860b76b7537d5f
Scott Braman <sbraman@redhat.com>
2023-10-06 21:35:24 -0400

    destroy cluster

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = ">= 4.20.0"
-#     }
-#     rhcs = {
-#       version = ">= 1.1.0"
-#       source  = "terraform-redhat/rhcs"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.20.0"
+    }
+    rhcs = {
+      version = ">= 1.1.0"
+      source  = "terraform-redhat/rhcs"
+    }
+  }
+}
 
-# provider "aws" {
-#   region = var.cloud_region  
-# }
+provider "aws" {
+  region = var.cloud_region  
+}
 
-# provider "rhcs" {
-#   token = var.token
-#   url   = var.url
-# }
+provider "rhcs" {
+  token = var.token
+  url   = var.url
+}
 
 # locals {
 #   sts_roles = {
