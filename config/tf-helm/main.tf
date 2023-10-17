800ec95f3dff3b0788f617d83080898c14ecf9f1
Scott Braman <sbraman@redhat.com>
2023-10-16 22:27:40 -0400

    updated main.tf

--- config/tf-helm/main.tf
+++ config/tf-helm/main.tf
@@ -8,17 +8,17 @@ data "shell_script" "token" {
 
 output "ocp_token" {
     value = data.shell_script.token.output["ocp_token"]
-    #sensitive   = true
+    sensitive   = true
 }
 
 # Comment out everything below here to destroy resources
-# resource "helm_release" "gitops" {
-#   name        = "gitops"
-#   chart       = "gitops"
-#   repository  = "."
-#   namespace   = "helm-gitops"
-#   max_history = 3
-#   create_namespace = true
-#   wait             = true
-#   reset_values     = true
-# }
+resource "helm_release" "gitops" {
+  name        = "gitops"
+  chart       = "gitops"
+  repository  = "."
+  namespace   = "helm-gitops"
+  max_history = 3
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+}
