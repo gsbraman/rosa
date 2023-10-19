239ab7e8a2d561e8a6fee430f77c48f0c57d0d42
Scott Braman <sbraman@redhat.com>
2023-10-18 22:42:34 -0400

    Unistall GitOps Operands

--- config/tf-helm/main.tf
+++ config/tf-helm/main.tf
@@ -12,24 +12,24 @@ output "ocp_token" {
 }
 
 # Comment out everything below here to destroy resources
-resource "helm_release" "gitops" {
-  name        = "gitops"
-  chart       = "gitops"
-  repository  = "."
-  namespace   = "helm-gitops"
-  max_history = 3
-  create_namespace = true
-  wait             = true
-  reset_values     = true
-}
+# resource "helm_release" "gitops" {
+#   name        = "gitops"
+#   chart       = "gitops"
+#   repository  = "."
+#   namespace   = "helm-gitops"
+#   max_history = 3
+#   create_namespace = true
+#   wait             = true
+#   reset_values     = true
+# }
 
-resource "helm_release" "gitops-operands" {
-  name        = "gitops-operands"
-  chart       = "gitops-operands"
-  repository  = "."
-  namespace   = "helm-gitops-operands"
-  max_history = 3
-  create_namespace = true
-  wait             = true
-  reset_values     = true
+# resource "helm_release" "gitops-operands" {
+#   name        = "gitops-operands"
+#   chart       = "gitops-operands"
+#   repository  = "."
+#   namespace   = "helm-gitops-operands"
+#   max_history = 3
+#   create_namespace = true
+#   wait             = true
+#   reset_values     = true
 # }
