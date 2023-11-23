c4683de441f1c17f050d3095c2d6052ab7d03c71
Scott Braman <sbraman@redhat.com>
2023-11-22 19:40:53 -0500

    Install GitOps Operands

--- config/tf-helm/main.tf
+++ config/tf-helm/main.tf
@@ -24,13 +24,13 @@ resource "helm_release" "gitops" {
 }
 
 
-# resource "helm_release" "gitops-operands" {
-#   name        = "gitops-operands"
-#   chart       = "gitops-operands"
-#   repository  = "."
-#   namespace   = "helm-gitops-operands"
-#   max_history = 3
-#   create_namespace = true
-#   wait             = true
-#   reset_values     = true
-# }
+resource "helm_release" "gitops-operands" {
+  name        = "gitops-operands"
+  chart       = "gitops-operands"
+  repository  = "."
+  namespace   = "helm-gitops-operands"
+  max_history = 3
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+}
